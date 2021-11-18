71ec47e1c535e82443bedfd69f9e54ca113bdfab
Aaron Kable <aaronkable@gmail.com>
2021-11-18 20:24:36 +0800

    add me command

--- fatfilters/auth_hooks.py
+++ fatfilters/auth_hooks.py
@@ -12,3 +12,8 @@ from .models import FATInTimePeriod
 @hooks.register('secure_group_filters') # this is the critical name we are searching for.
 def filters(): # can be any name
     return [FATInTimePeriod] # pass in the model classes as an array.
+
+
+@hooks.register('discord_cogs_hook')
+def register_cogs():
+    return ["fatfilters.fatcog"]
---
+++ fatfilters/fatcog.py
@@ -0,0 +1,71 @@
+# Cog Stuff
+from datetime import timedelta
+from discord.ext import commands
+from discord.embeds import Embed
+from discord.colour import Color
+# AA Contexts
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+from allianceauth.eveonline.models import EveCharacter
+# AA-Discordbot
+from aadiscordbot.cogs.utils.decorators import sender_has_perm
+from allianceauth.services.modules.discord.models import DiscordUser
+
+from afat.models import AFat
+
+import re
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_site_url():  # regex sso url
+    regex = r"^(.+)\/s.+"
+    matches = re.finditer(regex, settings.ESI_SSO_CALLBACK_URL, re.MULTILINE)
+    url = "http://"
+
+    for m in matches:
+        url = m.groups()[0] # first match
+
+
+class FatCog(commands.Cog):
+    """
+    All about fats!
+    """
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def me(self, ctx):
+        start_time = timezone.now() - timedelta(days=90)
+        user = DiscordUser.objects.get(uid=ctx.message.author.id).user
+        character_list = user.character_ownerships.all()
+        fats = AFat.objects.filter(character__in=character_list.values("character"), afatlink__afattime__gte=start_time) \
+            .order_by("afatlink__afattime")
+        fat_count = fats.count()
+        ships = fats.values_list('shiptype', flat=True)
+        last_fleet = fats.first().afatlink
+        last_date = last_fleet.afattime.strftime("%Y-%m-%d %H:%M")
+        last_message = f"{last_fleet.character}: {last_fleet.fleet} ({last_date})"
+        embed = Embed()
+        embed.title = "Recent FAT Activity"
+        embed.description = f"[Plese check auth for more info!]({get_site_url()})"
+
+        embed.add_field(name="Last 3 Months",
+                        value=fat_count, 
+                        inline=False)
+        embed.add_field(name="Ships",
+                        value=", ".join(ships), 
+                        inline=False)
+        embed.add_field(name="Last Fleet",
+                        value=last_message, 
+                        inline=False)
+        await ctx.message.author.send(embed=embed)
+        if ctx.guild is not None:
+            return await ctx.message.delete()
+
+def setup(bot):
+    bot.add_cog(FatCog(bot))
